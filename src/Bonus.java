b52e0571c455f649eba2c3073884eff4670b688e
Ekaterina Hlebalova <khlebalova.e@yandex.ru>
2020-06-11 19:25:47 +0400

    initial commit

---
+++ src/Bonus.java
@@ -0,0 +1,10 @@
+public class Bonus {
+    public static void main(String[] args) {
+        int ticketPrice = 1060;
+        int amountBonus = 20;
+        int miles = ticketPrice / amountBonus;
+        System.out.println(miles);
+
+    }
+
+}
